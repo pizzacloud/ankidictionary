7768e99bf54b3180e42a92a05bab1cdf616ee0dc
pizzacloud <54195144+pizzacloud@users.noreply.github.com>
2019-09-10 10:16:38 +0200

    Update esen_Collins.js

--- esen_Collins.js
+++ esen_Collins.js
@@ -39,50 +39,6 @@ class esen_Collins {
 
         let content = doc.querySelector('.content') || '';
         if (!content) return null;
-        let css = this.renderCSS();
-        return css + content.innerHTML;
-    }
-
-    renderCSS() {
-        let css = `
-            <style>
-                .copyright{
-                    display:none;
-                }
-                .orth {
-                    font-size: 100%;
-                    font-weight: bold;
-                }
-                .quote {
-                    font-style: normal;
-                    color: #1683be;
-                }
-                .colloc {
-                    font-style: italic;
-                    font-weight: normal;
-                }
-                .sense {
-                    /*border: 1px solid;*/
-                    /*border-color: #e5e6e9 #dfe0e4 #d0d1d5;*/
-                    border-radius: 3px;
-                    padding: 5px;
-                    margin: 5px 0;
-                    background-color: #f6f6f6;
-                }
-                .sense .re {
-                    font-size: 100%;
-                    margin-left: 0;
-                }
-                a {
-                    color: #000;
-                    text-decoration: none;
-                }
-                * {
-                    word-wrap: break-word;
-                    box-sizing: border-box;
-                }
-            </style>`;
-
-        return css;
+      
     }
 }
